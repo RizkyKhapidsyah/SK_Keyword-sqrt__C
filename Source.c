f93dc1adbdbe1fc85b32c034d084848697ea51c4
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-22 17:39:22 +0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+    float r;
+
+    r = sqrt(2.0);
+
+	printf("The square root of 2 is %f\n",r);
+
+	_getch();
+	return(0);
+}
